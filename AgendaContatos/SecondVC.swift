a414e5a3c573584552ac40ce2db4146f4b6cd4e6
JohnnyLutz <johnnylutz@JohnnyLutzs-Mac.local>
2019-11-22 11:22:25 -0800

    commit projeto

---
+++ AgendaContatos/SecondVC.swift
@@ -0,0 +1,124 @@
+//
+//  SecondVC.swift
+//  AgendaContatos
+//
+//  Created by JohnnyLutz on 22/11/19.
+//  Copyright © 2019 JohnnyLutz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SecondVC: UIViewController {
+
+    @IBOutlet weak var txtTelefone: UITextField!
+    @IBOutlet weak var txtNome: UITextField!
+    @IBOutlet weak var txtCategoria: UITextField!
+    
+    public var contato: NSManagedObject!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //carrega os dados do contato selecionado na tela para edição
+        if(contato != nil) {
+            txtNome.text = contato.value(forKey: "nome") as? String
+            txtTelefone.text = contato.value(forKey: "telefone") as? String
+            txtCategoria.text = contato.value(forKey: "categoria") as? String
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSalvar(_ sender: Any) {
+        if(contato == nil) {
+            save()
+        } else {
+            update()
+        }
+    }
+    
+    func save() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Contato",
+                                       in: managedContext)!
+        contato = NSManagedObject(entity: entity,
+                                      insertInto: managedContext)
+        
+        // 3
+        contato.setValue(txtNome.text, forKeyPath: "nome")
+        contato.setValue(txtTelefone.text, forKeyPath: "telefone")
+        contato.setValue(txtCategoria.text, forKeyPath: "categoria")
+        
+        // 4
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func update() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 3
+        contato.setValue(txtNome.text, forKeyPath: "nome")
+        contato.setValue(txtTelefone.text, forKeyPath: "telefone")
+        contato.setValue(txtCategoria.text, forKeyPath: "categoria")
+        
+        // 4
+        do {
+            try contato.managedObjectContext?.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func btnExcluir(_ sender: Any) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        managedContext.delete(contato)
+        
+        // 4
+        do {
+            try contato.managedObjectContext?.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AgendaContatos/ViewController.swift
+++ AgendaContatos/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreData
 
+class ViewController: UIViewController {
+    
+    var contatos: [NSManagedObject] = []
+    
+    @IBAction func btnCadastrar(_ sender: Any) {
+        //abre a tela de cadastro com os campos em branco para cadastro de um novo contato
+        //TO-DO
+        let myVC = storyboard?.instantiateViewController(withIdentifier: "SecondVC") as! SecondVC
+        navigationController?.pushViewController(myVC, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Contato")
+        
+        //3
+        do {
+            contatos = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return contatos.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            
+            let person = contatos[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell",
+                                              for: indexPath)
+            cell.textLabel?.text =
+                person.value(forKeyPath: "nome") as? String
+            cell.detailTextLabel?.text = person.value(forKeyPath: "telefone") as? String
+            return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let contato = contatos[indexPath.row]
+        let myVC = storyboard?.instantiateViewController(withIdentifier: "SecondVC") as! SecondVC
+        myVC.contato = contato
+        navigationController?.pushViewController(myVC, animated: true)
+    }
+
+}
